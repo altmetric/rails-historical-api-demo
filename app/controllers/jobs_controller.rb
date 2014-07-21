76f1c0606e07a6092171cf57ced906e513191bdf
Eric Wendelin <ewendelin@twitter.com>
2014-07-21 12:54:43 -0600

    Reduce method complexity in JobsController

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,15 +10,6 @@ class JobsController < ApplicationController
     render text: e.message, status: :bad_request
   end
 
-  SNAPSHOT_STATUSES = [
-      ['opened', 'estimating', 'auto_estimating', 'auto_estimate_failed', 'auto_estimate_running'], #estimating:
-      ['auto_estimate_complete', 'quoted', 'paused'], #ready:
-      ['accepted', 'running', 'completed', 'failed', 'validating', 'invalidated'], #running:
-      ['delivered', 'finished'], #complete:
-      ['rejected'] #rejected:
-  ]
-  VISIBLE_JOB_PHASES = ['#estimating', '#ready', '#running', '#complete', '#rejected']
-
   def index
     # Ignore Rejected jobs - no user action possible
     interesting_jobs = Gnip::HistoricalService.get_all_jobs[:jobs].select { |j| j[:status] != 'rejected' }
@@ -30,11 +21,8 @@ class JobsController < ApplicationController
   end
 
   def create
-    if params[:rules] != "" && params[:rulesFile]
-      raise Gnip::InvalidRequestException.new('Cannot specify both a rules file and raw rules input')
-    elsif params[:rules] == "" && params[:rulesFile].nil?
-      raise Gnip::InvalidRequestException.new('Please provide a rules file or raw rules input')
-    end
+    prevent_raw_and_file_rules_input!(params[:rules], params[:rulesFile])
+    ensure_raw_or_file_rules_input!(params[:rules], params[:rulesFile])
 
     if params[:rules] != ""
       rules_hash = params[:rules].split(/\r?\n/).map { |rule| {value: rule} }
@@ -78,27 +66,62 @@ class JobsController < ApplicationController
     render json: Yajl::Encoder.encode(hash, {html_safe?: true})
   end
 
+  def prevent_raw_and_file_rules_input!(rules_raw, rules_file)
+    if rules_raw != "" && rules_file
+      raise Gnip::InvalidRequestException.new('Cannot specify both a rules file and raw rules input')
+    end
+  end
+
+  def ensure_raw_or_file_rules_input!(rules_raw, rules_file)
+    if rules_raw == "" && rules_file.nil?
+      raise Gnip::InvalidRequestException.new('Please provide a rules file or raw rules input')
+    end
+  end
+
   def augment_job_data!(job)
     # Include list of past and in progress job phases - e.g. (an accepted job would be ['estimating', 'ready', 'running'])
     job[:uuid] = job[:jobURL].match('\/(\w+)\.json$')[1]
 
-    SNAPSHOT_STATUSES.each_with_index do |status_group, i|
-      if status_group.include?(job[:status])
-        job[:phases] = VISIBLE_JOB_PHASES[0, i+1]
-        job[:visible_status] = VISIBLE_JOB_PHASES[i]
-      end
-    end
+    job = add_job_status_detail!(job)
 
     # Add estimated runtime to running jobs
-    if job[:status] == 'running' && job[:percentComplete].to_f > 0
-      hours_running = (DateTime.now.utc - DateTime.iso8601(job[:acceptedAt])).to_f / 60
-      job[:timeRemainingHrs] = ((100.0 / job[:percentComplete].to_f - 1) * hours_running).round(1)
+    if job[:status] == 'running'
+      job = add_estimated_runtime!(job)
     else
       job.delete(:percentComplete)
     end
 
     # Omit irrelevant sections of API response
     job.delete(:quote) if %w(#running #complete #rejected).include?(job[:visible_status])
+    job
+  end
+
+  def add_job_status_detail!(job)
+    snapshot_statuses = [
+        ['opened', 'estimating', 'auto_estimating', 'auto_estimate_failed', 'auto_estimate_running'], #estimating:
+        ['auto_estimate_complete', 'quoted', 'paused'], #ready:
+        ['accepted', 'running', 'completed', 'failed', 'validating', 'invalidated'], #running:
+        ['delivered', 'finished'], #complete:
+        ['rejected'] #rejected:
+    ]
+    visible_job_phases = ['#estimating', '#ready', '#running', '#complete', '#rejected']
+
+    snapshot_statuses.each_with_index do |status_group, i|
+      if status_group.include?(job[:status])
+        job[:phases] = visible_job_phases[0, i+1]
+        job[:visible_status] = visible_job_phases[i]
+      end
+    end
+
+    job
+  end
+
+  def add_estimated_runtime!(job)
+    if job[:percentComplete].to_f > 0
+      hours_running = (DateTime.now.utc - DateTime.iso8601(job[:acceptedAt])).to_f / 60
+      job[:timeRemainingHrs] = ((100.0 / job[:percentComplete].to_f - 1) * hours_running).round(1)
+    end
+
     job
   end
 end
